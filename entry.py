4b0bd3a787ce8e9e585c836bffc81096b309ad90
Nikhil Sharma <104161695+NIKHIL-FORTUNIK@users.noreply.github.com>
2023-07-08 13:54:35 +0530

    Create entry.py

---
+++ entry.py
@@ -0,0 +1,56 @@
+import time
+a=input("enter your name:")
+b=int(input("enter number:"))
+
+if b<1000000000000000000:
+       time.sleep(2)
+       print("hello,beautyful....""miss",a)
+       time.sleep(2)
+       print("kese ho aap sab thek....")
+       time.sleep(2)
+       print("i hope apki journey achi gai hogi..")
+       time.sleep(3)
+       print("is situation me kuch yaad aa raha hai..")
+       time.sleep(1)
+       print("suno ge?")
+       print("yes/no")
+       time.sleep(3)
+       print("press 1 for yes and 2 for no")
+       x=int(input("enter choice:"))
+       if x==1:
+           time.sleep(2)
+           print("       mere toh sirf itni si kahani hai,")
+           print("       20% mai bachpan se kaminaa tha,")
+           print("       baki 80% apki meharbani hai....")
+           time.sleep(10)
+           print("#i have a small gift for you")
+           time.sleep(1)
+           print("##write your name to know:")
+           x=input("naam likho apna:")
+           
+           if x==a:
+               time.sleep(1)
+               print("5.....")
+               time.sleep(1)
+               print("4....")
+               time.sleep(1)
+               print("3....")
+               time.sleep(1)
+               print("2....")
+               time.sleep(1)
+               print("1....")
+               time.sleep(1)
+               print("lets start .... dont be shock:")
+               time.sleep(2)
+               print()
+                     
+       while(True):
+             print("hahahahahahahhahahhahahahhahahahahahahaahahhahahahahahhahahahahahahahhhahahhahahhahahahahaahahahahahahahahahhaahhahahaahahahahahahahahahahahahahhahahahahahhahahahahahahahahhahahahahahhahahahahahahahahhahahahahahahahahahahhahahahahhahahahahahahahhahhahahhahahahahahahahahahahahahhahahahahahhahahahhahahhahahahahahhahahahahhahahahahhhahahhahahahhahahahahahahahhahahahahhahahahahahhahahahahhahahahahhhahahahahhahahahahahhahahahhahahahahhahahahhahahhahahahahahhahahahahahahhahahahahahhahahahahahhaha")
+           
+                       
+       else:
+           print("program phirse start karo..kyuki apne no bola")
+    
+          
+else:
+       print("kya baat hai")
